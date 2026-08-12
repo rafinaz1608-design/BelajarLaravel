035182d0140a65e7102e712f7ff3cb8df9399c53
vino <vienorashad104@gmail.com>
2026-08-12 11:26:16 +0700

    dashboard admin slebew kiw kiw

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => bcrypt('password'),
+            ]
+        );
+
+        $this->call([
+            ClientSeeder::class,
+            ProjectSeeder::class,
+            ServiceSeeder::class,
+            TestimonialSeeder::class,
         ]);
     }
 }
